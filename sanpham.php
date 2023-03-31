ed99be75cbddb2fa175c36093b4043df82ba16d5
dangchinh9707 <113720181+dangchinh9707@users.noreply.github.com>
2023-04-01 03:29:18 +0700

    Add files via upload

---
+++ sanpham.php
@@ -0,0 +1,37 @@
+<div class=" container">
+            <div class="boxtrai mr">  
+                <div class="row mb">
+                       
+                <div class="boxtitle row">SẢN PHẨM CÙNG LOẠI <strong><?$tendm?></strong></div>
+                <div class="row boxcontent">
+                    <?php
+                        $i=0;
+                        foreach ($dssp as $sp) {
+                            extract($sp);
+                            $linksp="index.php?act=sanphamct&idsp=".$id;
+                            $hinh=$img_path.$img;
+                            if(($i==2)||($i==5)||($i==8)||($i=11)){
+                                $mr="";
+                            }else {
+                                $mr="mr";
+                            }
+                            echo '<div class="box_sanpham '.$mr.'">
+                                        <div class=" row img"><a href="'.$linksp.'"><img src="'.$hinh.'" alt=""></a></div>
+                
+                                        <p>Giá KM: '.$price.' Đ</p>
+                                        <a href="'.$linksp.'">'.$name.'</a>
+                                    </div>';
+                            $i+=1;
+                        }
+                    ?>
+                    
+                    
+                    
+                        </div>
+                    </div>
+                </div>
+
+                <div class="boxphai">
+                    <?php include "boxright.php"?>;
+                </div>
+        </div>
